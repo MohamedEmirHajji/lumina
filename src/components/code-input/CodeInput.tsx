d432e522b7ef873e72ea2b2eac1b7d7af7871f59
MohamedEmirHajji <hajji.mohamedemir@outlook.com>
2025-02-18 22:57:28 +0100

    feat: add CodeInput component

---
+++ src/components/code-input/CodeInput.tsx
@@ -0,0 +1,11 @@
+import {ReactElement} from "react";
+import './CodeInput.css';
+
+function CodeInput(): ReactElement {
+    return (<div>
+        <div></div>
+        <textarea rows={37}></textarea>
+    </div>)
+}
+
+export default CodeInput;
