a3cd3afacae83da16119766a951ea802795b3192
MohamedEmirHajji <hajji.mohamedemir@outlook.com>
2025-02-18 12:49:42 +0100

    chore: fix loading problem

--- electron/main.ts
+++ electron/main.ts
@@ -1,58 +1,56 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
-import * as isDev from 'electron-is-dev';
 import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 
-let win: BrowserWindow | null = null;
-
 function createWindow() {
-    win = new BrowserWindow({
+    const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true
+            // contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js')
         }
     })
 
-    if (isDev) {
-        win.loadURL('http://localhost:3000/index.html');
-    } else {
+    if (app.isPackaged) {
         // 'build/index.html'
         win.loadURL(`file://${__dirname}/../index.html`);
-    }
+    } else {
+        win.loadURL('http://localhost:3000/index.html');
 
-    win.on('closed', () => win = null);
+        win.webContents.openDevTools();
 
-    // Hot Reloading
-    if (isDev) {
-        // 'node_modules/.bin/electronPath'
+        // Hot Reloading on 'node_modules/.bin/electronPath'
         require('electron-reload')(__dirname, {
-            electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
+            electron: path.join(__dirname,
+                '..',
+                '..',
+                'node_modules',
+                '.bin',
+                'electron' + (process.platform === "win32" ? ".cmd" : "")),
             forceHardReset: true,
             hardResetMethod: 'exit'
         });
     }
+}
 
+app.whenReady().then(() => {
     // DevTools
     installExtension(REACT_DEVELOPER_TOOLS)
         .then((name) => console.log(`Added Extension:  ${name}`))
         .catch((err) => console.log('An error occurred: ', err));
 
-    if (isDev) {
-        win.webContents.openDevTools();
-    }
-}
+    createWindow();
 
-app.on('ready', createWindow);
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (win === null) {
-        createWindow();
-    }
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
 });
